8bf47182f3444a645e2165d2f600c59a9ce3ccea
crowdbotics-dev <engineering@crowdbotics.com>
2023-07-25 13:31:12 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Person(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=255,)
+    icon = models.ImageField(upload_to="person/icon/images/",null=True,blank=True,)
